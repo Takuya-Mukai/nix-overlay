12b6bc4739f1871a66ce4c7e99fde7b37d841f34
Takuya-Mukai <takuya.mukai@gmail.com>
2025-11-09 23:50:22 +0900

    first commit

---
+++ flake.nix
@@ -0,0 +1,104 @@
+{
+  description = "A flake for managing custom packages and development environments";
+
+  inputs = {
+    # nixpkgs をインプットとして定義
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      # サポートするシステム（アーキテクチャ）のリスト
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      # 各システムごとに pkgs を生成するためのヘルパー関数
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      # 各システム用の nixpkgs のパッケージセット
+      pkgsFor = system: nixpkgs.legacyPackages.${system};
+
+      neopyterFunction = import ./pkgs/neopyter.nix;
+      # ex)
+      # anotherPkgFunction = import ./pkgs/another.nix;
+
+    in
+    {
+      # --- 1. パッケージ定義 (nix build 用) ---
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          # ここで {pkgs} を渡して関数を呼び出し、ビルドする
+          neopyter = neopyterFunction { inherit pkgs; };
+          # another = anotherPkgFunction { inherit pkgs; };
+        }
+      );
+
+      # `nix build` (引数なし) のデフォルトパッケージ
+      # defaultPackage = forAllSystems (system: self.packages.${system}.another);
+
+      # --- 2. Overlay (パッケージの再利用) ---
+      # (★ 変更点)
+      overlays.default = final: prev: {
+
+        # --- A. 通常のパッケージ (pkgs のトップレベルに追加) ---
+        # another = anotherPkgFunction { pkgs = final; };
+
+        python3Packages = prev.python3Packages // {
+          neopyter = neopyterFunction { pkgs = final; };
+        };
+      };
+
+      # --- 3. 開発環境 (複数) ---
+      devShells = forAllSystems (
+        system:
+        let
+          # (★ 変更なし)
+          # この Flake 自身が開発環境で overlay を使う
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
+
+          # Python 環境の定義
+          dataAnalysisPython = pkgs.python3.withPackages (ps: [
+            ps.jupyterlab
+            ps.numpy
+            ps.pandas
+            ps.matplotlib
+            ps.scipy
+            ps.seaborn
+            ps.plotly
+            ps.scikit-learn
+            ps.neopyter
+          ]);
+        in
+        {
+          dataAnalysis = pkgs.mkShell {
+            packages = [
+              dataAnalysisPython
+              # pkgs.another
+            ];
+          };
+
+          # (例) リポジトリ管理用のデフォルトシェル
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.bashInteractive
+              pkgs.git
+            ];
+          };
+        }
+      );
+
+      # devShell = forAllSystems (system: self.devShells.${system}.default);
+    };
+}
---
+++ pkgs/neopyter.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+
+pkgs.python3Packages.buildPythonPackage rec {
+  pname = "neopyter";
+  version = "0.3.2";
+
+  src = pkgs.python3Packages.fetchPypi {
+    inherit pname version;
+    # (sha256 に修正しました)
+    sha256 = "w5gOSKdRc163UPFmrf/SGtkKRU5C2KOGb6aR6RT0FiM=";
+  };
+
+  pyproject = true;
+
+  nativeBuildInputs = [
+    pkgs.python3Packages.hatchling
+    pkgs.python3Packages.hatch-jupyter-builder
+    pkgs.python3Packages.hatch-nodejs-version
+  ];
+
+  propagatedBuildInputs = [
+    pkgs.python3Packages.jupyterlab
+    pkgs.python3Packages.pynvim
+  ];
+
+  doCheck = true;
+}
